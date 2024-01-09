663e792e6fc87f6943ec9d482c8d06e577ef0853
Asfandyar <fandiyar393@gmail.com>
2024-01-09 08:21:13 -0500

    Add files via upload
    Backend Files

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const connect = require("./src/Configs/db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: true,
+  exposedHeaders: ["Content-Length", "Bearer"]
+}));
+
+const userConroller = require("./src/Controllers/userController");
+const cookController = require("./src/Controllers/cookController");
+
+app.use("/auth", userConroller);
+
+app.use("/cook", cookController);
+
+app.listen(process.env.PORT || 8080, async () => {
+  try {
+    await connect();
+    console.log("Server started");
+  } catch (error) {
+    console.log({ error });
+    console.log("Unable to start server");
+  }
+});
---
+++ Backend/src/Configs/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// connect scope for connecting with database
+const connect = () => {
+  // return mongoose.connect("mongodb+srv://cookAuth:deepanshu@cluster0.2sw65.mongodb.net/chef?retryWrites=true&w=majority"
+  return mongoose.connect("mongodb+srv://rosx:Rosx@geek.ogw8bdm.mongodb.net/chef?retryWrites=true&w=majority"
+    // `${process.env.DB}`,
+  );
+};
+
+// exporting connect scople
+module.exports = connect;
